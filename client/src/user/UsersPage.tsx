e59b602f97669ff00e2e721584ed1132cc3ca1e4
Preston-D <prestonduffield1@gmail.com>
2024-01-20 14:50:30 -0800

    user page, cors

--- client/src/user/UsersPage.tsx
+++ client/src/user/UsersPage.tsx
@@ -1,13 +1,38 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const UsersPage: React.FC = () => {
-  return (
-    <>
-      <h1>Page for all Users</h1>
-      <Link to="/">Home</Link>
-    </>
-  );
+  const [users, setUsers] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    //TODO: Pull out into environement var
+    fetch("http://localhost:3000/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+  if (isLoading) return <p>Loading...</p>;
+  if (!users) {
+    return <p>No profile data</p>;
+  } else {
+    return (
+      <ul key="userList">
+        {(users as any).map((user: any) => {
+          return (
+            <li key={user._id}>
+              Name: {user.firstName} {user.lastName}, email: {user.email}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 };
 
 export default UsersPage;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,42 @@
-import 'dotenv/config'
-
-import express from 'express'
-const app = express()
-import mongoose from 'mongoose'
+import "dotenv/config";
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import cors from "cors";
 
 // Mongoose Setup
-if(process.env.DATABASE_URL != null){
-  mongoose.connect(process.env.DATABASE_URL)
+if (process.env.DATABASE_URL != null) {
+  mongoose.connect(process.env.DATABASE_URL);
 } else {
-  console.log('Error fetching database URL')
+  console.log("Error fetching database URL");
 }
 
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connnected to Database'))
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connnected to Database"));
 // This should be set in production
 // mongoose.set('autoIndex', false);
 
 // Express Setup
-app.use(express.json())
-import {debugLogger} from './helpers/serverHelper.js'
-app.use(debugLogger)
+app.use(express.json());
+import { debugLogger } from "./helpers/serverHelper.js";
+app.use(debugLogger);
 
+//CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    // credentials: true,
+  })
+);
 // Routes
-import usersRouter from './routes/users.js'
-app.use('/users', usersRouter)
+import usersRouter from "./routes/users.js";
+app.use("/users", usersRouter);
 
-
-app.listen(3000,() => console.log('Server Started'))
+app.listen(3000, () => console.log("Server Started"));
 
 // Home Routes '/'
-app.get('/', (req, res, next) => {
-
-    res.send('Hello World!')
-    next()
-  })
+app.get("/", (req, res, next) => {
+  res.send("Hello World!");
+  next();
+});
